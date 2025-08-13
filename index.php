9ba4d2cc8e67ba8c7f539e0b67845769bc2ae9e3
Megane11 <meganedjiane@gmail.com>
2025-08-13 19:09:15 +0100

    Implement responsive search button with hover effect

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Presence marking</title>
+    <link rel="stylesheet" href="bootstrap-5.3.7-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style1.css">
+
+</head>
+<body>
+    <nav class="nav">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="assets/images/tagus-drone.webp" class="logo"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"  href="presence.php">PRESENCE LIST</a>
+        </li>
+        
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<script src="bootstrap-5.3.7-dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ presence.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PRESENCE LIST</title>
+  <link rel="stylesheet" href="assets/css/style1.css">
+</head>
+
+<body>
+  <nav class="nav">
+    <div class="logo-container">
+      <a class="navbar-brand" href="#">
+        <div ><img class="logo" src="assets/images/tagus logo modif.png" alt=""></div>
+      </a>
+    </div>
+    <div class="list">
+      <img class="print-icon" src="assets/images/print-icon.svg" alt="Print">
+      <form class="search-form" role="search">
+        <input class="search-input" type="search" placeholder="Search" aria-label="Search" />
+        <button class="search-submit" type="submit">
+          <span class="search-text">Search</span>
+          <img class="search-icon" src="assets/images/search-icon.svg" alt="Search Icon">
+        </button>
+      </form>
+    </div>
+  </nav>
+  <div class="date1">Date: August 14, 2025</div>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Post</th>
+        <th scope="col">Arrival time</th>
+        <th scope="col">Departure time</th>
+      </tr>
+    </thead>
+    <tbody class="table-body">
+      <tr>
+        <td>1</td>
+        <td>John Smith</td>
+        <td>Manager</td>
+        <td>08:45 AM</td>
+        <td>05:30 PM</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Sarah Johnson</td>
+        <td>Developer</td>
+        <td>09:15 AM</td>
+        <td>06:00 PM</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Michael Brown</td>
+        <td>Designer</td>
+        <td>09:00 AM</td>
+        <td>05:45 PM</td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>
